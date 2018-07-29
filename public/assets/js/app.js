d8d572dadee123c028c46840057823da0bd91fe6
Nick Akrap <nakrap@gmail.com>
2018-07-29 16:55:34 -0500

    working on css, bootstrap, adding attributes using javascript

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -3,8 +3,9 @@ $.getJSON("/articles", function(data) {
     // For each one
     for (var i = 0; i < data.length; i++) {
       // Display the apropos information on the page
-      $("#articles").append("<p data-id='" + data[i]._id + "'>" + "<a class= list-group-item list-group-item-action active href='" + data[i].link + "'>" + data[i].title + "<br />" + data[i].link + "</a>" + "</p>");
+      $("#articles").append("<p class= list-group-item list-group-item-action active data-id='" + data[i]._id + "'>" + data[i].title + "<br />" + "<a target=_blank href='" + data[i].link + "'>" + data[i].link + "</a>" + "</p>");
     }
+
     // console.log(data);
   });
   
@@ -25,14 +26,14 @@ $.getJSON("/articles", function(data) {
       .then(function(data) {
         // console.log(data);
         // The title of the article
-        $("#notes").append("<h2>" + data.title + "</h2>");
+        $("#notes").append("<h5>" + data.title + "</h5>");
         // An input to enter a new title
         $("#notes").append("<input id='titleinput' name='title' >");
         // A textarea to add a new note body
         $("#notes").append("<textarea id='bodyinput' name='body'></textarea>");
         // A button to submit a new note, with the id of the article saved to it
-        $("#notes").append("<button data-id='" + data._id + "' id='savenote'>Save Note</button>");
-  
+        $("#notes").append("<button type='button' class='btn btn-primary btn-lg btn-block' data-id='" + data._id + "' id='savenote'>Save Note</button>");
+ 
         // If there's a note in the article
         if (data.note) {
           // Place the title of the note in the title input
